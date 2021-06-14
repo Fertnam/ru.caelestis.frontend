91e920c0ea1da9e2f5a783e0e5e96e1706a48064
Fertnam <fertnam@mail.ru>
2021-06-14 16:50:16 +1000

    Добавлена валидация для форм смены пароля и email

--- src/components/default/views/profile/ChangeEmail.vue
+++ src/components/default/views/profile/ChangeEmail.vue
@@ -9,18 +9,49 @@
                 <div :class="formStyles.formItem">
                     <label :class="formStyles.formLabel">
                         Новый Email:
-                        <input :class="formStyles.formInput" type="text" />
+                        <input
+                            v-model="v$.email.$model"
+                            :class="formStyles.formInput"
+                            type="text"
+                        />
                     </label>
-                    <div v-if="false" :class="[formStyles.formMessage]" />
+                    <div
+                        v-if="v$.email.$error"
+                        :class="[formStyles.formMessage]"
+                    >
+                        <fa-icon
+                            :class="formStyles.formMessageIcon"
+                            icon="exclamation-circle"
+                        />
+
+                        <template v-if="v$.email.required.$invalid">
+                            Укажите email
+                        </template>
+
+                        <template v-else-if="v$.email.email.$invalid">
+                            Неверная структура email
+                        </template>
+                    </div>
                 </div>
 
                 <div :class="formStyles.formItem">
                     <label :class="formStyles.formLabel">
                         Пароль:
-                        <input :class="formStyles.formInput" type="password" />
+                        <input
+                            v-model="v$.password.$model"
+                            :class="formStyles.formInput"
+                            type="password"
+                        />
                     </label>
-                    <div v-if="false" :class="[formStyles.formMessage]">
-                        Пароль корректный
+                    <div
+                        v-if="v$.password.$error"
+                        :class="[formStyles.formMessage]"
+                    >
+                        <fa-icon
+                            :class="formStyles.formMessageIcon"
+                            icon="exclamation-circle"
+                        />
+                        Укажите пароль
                     </div>
                 </div>
 
@@ -30,10 +61,12 @@
     </ToggleContent>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
 import ToggleContent from '@default-components/ToggleContent.vue'
 import formStyles from '@default-scss-modules/form.module.scss'
+import useVuelidate from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
 
 export default defineComponent({
     name: 'ChangeEmail',
@@ -43,10 +76,25 @@ export default defineComponent({
     data() {
         return {
             formStyles,
+            v$: useVuelidate(),
+            email: '',
+            password: '',
+        }
+    },
+    validations() {
+        return {
+            email: { required, email },
+            password: { required },
         }
     },
     methods: {
         onSubmit() {
+            this.v$.$touch()
+
+            if (this.v$.$error) {
+                return
+            }
+
             alert('Email изменен')
         },
     },
--- src/components/default/views/profile/ChangePassword.vue
+++ src/components/default/views/profile/ChangePassword.vue
@@ -9,28 +9,77 @@
                 <div :class="formStyles.formItem">
                     <label :class="formStyles.formLabel">
                         Старый пароль:
-                        <input :class="formStyles.formInput" type="password" />
+                        <input
+                            v-model="v$.oldPassword.$model"
+                            :class="formStyles.formInput"
+                            type="password"
+                        />
                     </label>
-                    <div v-if="false" :class="[formStyles.formMessage]" />
+                    <div
+                        v-if="v$.oldPassword.$error"
+                        :class="[formStyles.formMessage]"
+                    >
+                        <fa-icon
+                            :class="formStyles.formMessageIcon"
+                            icon="exclamation-circle"
+                        />
+
+                        <template v-if="v$.oldPassword.required.$invalid">
+                            Укажите старый пароль
+                        </template>
+                    </div>
                 </div>
 
                 <div :class="formStyles.formItem">
                     <label :class="formStyles.formLabel">
                         Новый пароль:
-                        <input :class="formStyles.formInput" type="password" />
+                        <input
+                            v-model="v$.newPassword.$model"
+                            :class="formStyles.formInput"
+                            type="password"
+                        />
                     </label>
-                    <div v-if="false" :class="[formStyles.formMessage]">
-                        Пароль корректный
+                    <div
+                        v-if="v$.newPassword.$error"
+                        :class="[formStyles.formMessage]"
+                    >
+                        <fa-icon
+                            :class="formStyles.formMessageIcon"
+                            icon="exclamation-circle"
+                        />
+
+                        <template v-if="v$.newPassword.required.$invalid">
+                            Укажите новый пароль
+                        </template>
                     </div>
                 </div>
 
                 <div :class="formStyles.formItem">
                     <label :class="formStyles.formLabel">
                         Повторите новый пароль:
-                        <input :class="formStyles.formInput" type="password" />
+                        <input
+                            v-model="v$.newPasswordRepeat.$model"
+                            :class="formStyles.formInput"
+                            type="password"
+                        />
                     </label>
-                    <div v-if="false" :class="[formStyles.formMessage]">
-                        Пароль корректный
+                    <div
+                        v-if="v$.newPasswordRepeat.$error"
+                        :class="[formStyles.formMessage]"
+                    >
+                        <fa-icon
+                            :class="formStyles.formMessageIcon"
+                            icon="exclamation-circle"
+                        />
+
+                        <template v-if="v$.newPasswordRepeat.required.$invalid">
+                            Повторите новый пароль
+                        </template>
+                        <template
+                            v-else-if="v$.newPasswordRepeat.sameAs.$invalid"
+                        >
+                            Пароли не совпадают
+                        </template>
                     </div>
                 </div>
 
@@ -44,6 +93,8 @@
 import { defineComponent } from 'vue'
 import ToggleContent from '@default-components/ToggleContent.vue'
 import formStyles from '@default-scss-modules/form.module.scss'
+import useVuelidate from '@vuelidate/core'
+import { email, required, sameAs } from '@vuelidate/validators'
 
 export default defineComponent({
     name: 'ChangePassword',
@@ -53,10 +104,27 @@ export default defineComponent({
     data() {
         return {
             formStyles,
+            v$: useVuelidate(),
+            oldPassword: '',
+            newPassword: '',
+            newPasswordRepeat: '',
+        }
+    },
+    validations() {
+        return {
+            oldPassword: { required },
+            newPassword: { required },
+            newPasswordRepeat: { required, sameAs: sameAs(this.newPassword) },
         }
     },
     methods: {
         onSubmit() {
+            this.v$.$touch()
+
+            if (this.v$.$error) {
+                return
+            }
+
             alert('Пароль изменен')
         },
     },
